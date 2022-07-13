093f8e78dd733a26488016c4d13b091663c97a8f
Elliot T. S. (Windows VirtualBox) <elliotbb@believershome.com>
2022-07-13 15:45:36 -0500

    Made it work on Windows

--- compile_class.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-cd src/edu/wsu/KheperaSimulator
-javac -d edu $1.java
-cd ../../../..
-cp ./src/edu/wsu/KheperaSimulator/edu/edu/wsu/KheperaSimulator/$1.class ./classes/edu/wsu/KheperaSimulator
-
